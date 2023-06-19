710d08a601351d53cf68d848d4453bd6d962e047
Veronika Star <Starchevskaya.v@gmail.com>
2023-06-19 23:18:53 +0300

    check for ssh_my

--- src/ua/hilel/lesson3/ConditionExample.java
+++ src/ua/hilel/lesson3/ConditionExample.java
@@ -11,7 +11,7 @@ public class ConditionExample {
          if (a==b) {
              System.out.println("digits are same");
          } else {
-             System.out.println("");
+             System.out.println("ssh");
          };
          }
     }
